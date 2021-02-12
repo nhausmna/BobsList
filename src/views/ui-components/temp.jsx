4f42eaa706dfeacdbabb55ab7388d96fd84f3b5a
jphaws <jphaws@calpoly.edu>
2021-02-11 22:36:38 -0800

    Created a new listing form and temporary page to view it

---
+++ src/views/ui-components/temp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ContactForm from '../../components/forms/contactForm';
+import NewListingForm from '../../components/forms/newListingForm';
+
+const TempPage = () => {
+    return (
+        <div>
+            <ContactForm />
+            <NewListingForm />
+        </div>
+
+    );
+}
+
+export default TempPage;
