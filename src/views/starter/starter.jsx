ad5cf390aee043dea48afef6771a961438621238
nhausmna <35944399+nhausmna@users.noreply.github.com>
2021-02-16 18:54:52 -0800

    Modified cards to take in data and display such on the frontend. Exammples included

--- src/views/starter/starter.jsx
+++ src/views/starter/starter.jsx
@@ -27,36 +27,38 @@ import img5 from '../../assets/images/big/img5.jpg';
 import img6 from '../../assets/images/big/img6.jpg';
 import img7 from '../../assets/images/background/img5.jpg';
 
-const OneCard = () => {
-    return(
-        <Col xs="12" md="4">
-        <Card>
-            <CardImg top width="100%" src={img4} />
-            <CardBody>
-                <CardTitle>Example Product</CardTitle>
-                <CardSubtitle>$999</CardSubtitle>
-                <CardText>This is where the description will go</CardText>
-                <Button>Contact Seller</Button>
-            </CardBody>
-        </Card>
-    </Col>
-    )
-}
-
-const Cards = () => {
-    const cards = [<OneCard />, <OneCard />, <OneCard />, <OneCard />, <OneCard />, <OneCard />, <OneCard />, <OneCard />, <OneCard />]
+const Cards = props => {
+    const cards = [
+        <OneCard title="EXAMPLE PRODUCT" price="$34" description="this will be a desc" image={img3}/>,
+        <OneCard title="EXAMPLE PRODUCT 2" price="$3431" description="this will be a desc 2" image={img7}/>
+    ]
 
     return (
         <div>
-            {/* --------------------------------------------------------------------------------*/}
-            {/* Row*/}
-            {/* --------------------------------------------------------------------------------*/}
             <h5 className="mb-3">Listings</h5>
             <Row>
                 {cards}
             </Row>
         </div>
     );
+}
+
+const OneCard = data => {
+    return(
+        <Col xs="12" md="4">
+        <Card>
+            <CardImg top width="100%" src={data.image} />
+            <CardBody>
+                <CardTitle>{data.title}</CardTitle>
+                <CardSubtitle>{data.price}</CardSubtitle>
+                <CardText>{data.description}</CardText>
+                <Button className="btn" color="info" size="md" href="#/contact_seller" active>
+                                    Contact Seller
+                  </Button>
+            </CardBody>
+        </Card>
+    </Col>
+    )
 }
 
 export default Cards;
