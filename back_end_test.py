128ccfcf5c4d281864fd827ea83d657616fe5397
Edwin Olivares <edolivar@calpoly.edu>
2021-02-22 12:45:55 -0800

    Moving the files out of the backend folder

--- back_end_test.py
+++ back_end_test.py
@@ -1,34 +1,32 @@
 import pytest
-import back_end
 
 def test_get_listings():
-      expected = {
-  "listings_list": [
-    {
-      "_id": "602cc42ae04d5011230f8848", 
-      "description": "edjeauhdeiuadeuadheuahdiehaduiehadueiaud", 
-      "distance": "5", 
-      "name": "Edwin", 
-      "p_name": "iPhone", 
-      "price": "$10.00"
-    }, 
-    {
-      "_id": "602d7ffbe04d5011230f8849", 
-      "description": "edjeauhdeiuadeuadheuahdiehaduiehadueiaud", 
-      "distance": "6", 
-      "name": "Jaxon", 
-      "p_name": "TV", 
-      "price": "$100.00"
-    }, 
-    {
-      "_id": "602d805fe04d5011230f884a", 
-      "description": "edjeauhdeiuadeuadheuahdiehaduiehadueiaud", 
-      "distance": "3", 
-      "name": "Nick", 
-      "p_name": "Shoes", 
-      "price": "$25.00"
-    }
-  ]
-}
-      
+    expected = {
+        "listings_list": [
+            {
+            "_id": "602cc42ae04d5011230f8848", 
+            "description": "edjeauhdeiuadeuadheuahdiehaduiehadueiaud", 
+            "distance": "5", 
+            "name": "Edwin", 
+            "p_name": "iPhone", 
+            "price": "$10.00"
+            }, 
+            {
+            "_id": "602d7ffbe04d5011230f8849", 
+            "description": "edjeauhdeiuadeuadheuahdiehaduiehadueiaud", 
+            "distance": "6", 
+            "name": "Jaxon", 
+            "p_name": "TV", 
+            "price": "$100.00"
+            }, 
+            {
+            "_id": "602d805fe04d5011230f884a", 
+            "description": "edjeauhdeiuadeuadheuahdiehaduiehadueiaud", 
+            "distance": "3", 
+            "name": "Nick", 
+            "p_name": "Shoes", 
+            "price": "$25.00"
+            }
+        ]
+    }   
     assert back_end.get_listings() == expected
