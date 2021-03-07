b03cdeef9e82a24a64e3d8708ad4abb7a0f51da2
jphaws <jphaws@calpoly.edu>
2021-03-06 21:20:57 -0800

    Added support for messages between seller & buyer

---
+++ src/layouts/inbox.jsx
@@ -0,0 +1,104 @@
+import React, {Component, useState} from 'react';
+import axios from 'axios';
+import MessageAlert from '../views/ui-components/messageAlert.jsx'
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    Col, 
+    Row
+} from 'reactstrap';
+
+class Inbox extends Component{
+    state = {
+        messages_to: [],
+        messages_from: []
+    }
+
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/inbox?to=' + sessionStorage.getItem('username'))
+            .then(res => {
+                const messages_to = res.data.messages_list;
+                this.setState({ messages_to });
+            })
+            .catch(function (error) {
+                //Not handling the error. Just logging into the console.
+                console.log(error);
+            });
+        axios.get('http://localhost:5000/inbox?from=' + sessionStorage.getItem('username'))
+            .then(res => {
+                const messages_from = res.data.messages_list;
+                this.setState({ messages_from });
+            })
+            .catch(function (error) {
+                //Not handling the error. Just logging into the console.
+                console.log(error);
+            });
+    }
+
+    render(){
+        console.log(this.state)
+        const {messages_to} = this.state;
+        const alerts_to_array = []
+
+        const {messages_from} = this.state;
+        const alerts_from_array = []
+
+        for (let m of messages_to){
+            alerts_to_array.push(<MessageAlert
+                post_title={m.post_title}
+                from={m.from}
+                message={m.message}
+                phonenumber={m.phonenumber}
+                email={m.email}
+                _id={m._id}
+                item_id={m.item_id}
+                to={m.to}
+                >
+            </MessageAlert>)
+        }
+        for (let m of messages_from){
+            alerts_from_array.push(<MessageAlert
+                post_title={m.post_title}
+                from={m.from}
+                message={m.message}
+                phonenumber={m.phonenumber}
+                email={m.email}
+                _id={m._id}
+                item_id={m.item_id}
+                to={m.to}
+                >
+            </MessageAlert>)
+        }
+
+        return(
+            <Row>
+                <Col sm="6">
+                    <Card>
+                        <CardHeader>Messages Received</CardHeader>
+                        <CardBody>                            
+                            <div>
+                                {alerts_to_array}
+                            </div>
+                                
+                        </CardBody>
+                    </Card>
+                </Col>
+            
+                <Col sm="6">
+                 <Card>
+                    <CardHeader>Messages Sent</CardHeader>
+                        <CardBody>                            
+                            <div> {alerts_from_array} </div>
+                        </CardBody>
+                 </Card>
+                </Col>
+            </Row>
+            
+        )
+    }
+    
+}
+
+export default Inbox;
